0ad0b433bb7bb45e79eace1fb52bfbc98455247c
martincomito <martin.l.comito@accenture.com>
2019-03-14 20:59:43 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,138 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.react.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
+
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      list: list,
+      searchTerm: ''
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event){
+    this.setState({ searchTerm: event.target.value })
+    console.log(event.target.value)
+  }
+
+  onDismiss(id){
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({list: updatedList})
+  }
+
   render() {
+    const { 
+      list, 
+      searchTerm 
+    } = this.state;
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="page">
+        <div className="interactions">
+          <Search
+          value={searchTerm}
+          onChange={this.onSearchChange} 
           >
-            Learn React
-          </a>
-        </header>
+          Search
+          </Search>
+        </div>
+
+        <Table
+        list={list}
+        pattern={searchTerm}
+        onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
 }
+
+const Search = ({ value, onChange, children }) =>
+      <form>
+        <label>{children}</label>
+        <input 
+        type="text"
+        value={value}
+        onChange={onChange}
+        />
+      </form>
+
+
+//  CSS columns width
+const largeColumn = {
+  width: '40%',
+}
+
+const midColumn = {
+  width: '30%',
+}
+
+const smallColumn = {
+  width: '10%',
+}
+
+const Table = ({ list, pattern, onDismiss }) =>
+      <div className="table">
+        {list.filter(isSearched(pattern)).map(item => 
+        <div key={item.objectID} className="table-row">
+          <span style={ largeColumn }>
+            <a href={item.url}>{item.title} </a>
+          </span>
+          <span style={ midColumn }>
+            {item.author} 
+          </span>
+          <span style={ smallColumn }>
+            {item.num_comments} 
+          </span>
+          <span style={ smallColumn }>
+            {item.points}
+          </span>
+          <span style={ smallColumn }>
+            <Button 
+              onClick={() => onDismiss(item.objectID)}
+              className="button-inline"
+            >
+              Dismiss
+            </Button>
+          </span>
+        </div>
+        )}
+      </div>
+
+const Button = ({ onClick, className = '', children }) =>
+     <button 
+     onClick={onClick}
+     className={className}
+     type="button"
+     >
+      {children}  
+     </button>
 
 export default App;
